b9a6cc6ab7a00d4e51ba8e016ccb40f90a903f7a
zhangfengliu <809557950@qq.com>
2021-06-27 15:11:43 +0800

    first commit

---
+++ intergration/src/main/java/com/example/demo/IntergretionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntergretionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntergretionApplication.class, args);
+    }
+
+}
